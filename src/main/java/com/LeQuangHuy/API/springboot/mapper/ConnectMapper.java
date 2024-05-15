57b0bd5d0ef053c4cc9a5dba4853f2b0e3834420
QuangHuy <lehuyprocuber@gmail.com>
2024-05-15 16:27:13 +0700

    Update ConnectMapper.java

--- src/main/java/com/LeQuangHuy/API/springboot/mapper/ConnectMapper.java
+++ src/main/java/com/LeQuangHuy/API/springboot/mapper/ConnectMapper.java
@@ -1,4 +1,14 @@
-package com.LeQuangHuy.API.springboot.mapper;
 
-public class ConnectMapper {
+import com.LeQuangHuy.API.springboot.model.Connect;
+import com.LeQuangHuy.API.springboot.dto.ConnectDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ConnectMapper {
+
+    @Mapping(source = "user.id", target = "userId")
+    ConnectDTO connectToDTO (Connect connect);
+    Connect toEntity(ConnectDTO connectDTO);
 }
