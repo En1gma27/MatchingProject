e74b3ba1dc6fdd60282db27d67d02ad479f0abb6
QuangHuy <lehuyprocuber@gmail.com>
2024-05-15 16:45:15 +0700

    Update ConnectRepository.java

--- src/main/java/com/LeQuangHuy/API/springboot/repository/ConnectRepository.java
+++ src/main/java/com/LeQuangHuy/API/springboot/repository/ConnectRepository.java
@@ -1,15 +1,14 @@
-//package com.LeQuangHuy.API.springboot.repository;
-//
-//import com.LeQuangHuy.API.springboot.model.Connect;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//
-//public interface ConnectRepository extends JpaRepository<Character, Long> {
-//
-//	List<Character> findConnectByUserID (Long userId);
-//
-//
-//
-//}
+import com.LeQuangHuy.API.springboot.model.Connect;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface ConnectRepository extends JpaRepository<Connect, Long> {
+
+    List<Connect> findByUserId (Long userId);
+    List<Connect> findByType(String type);
+    List<Connect> findByUserIdAndType(Long userId, String type);
+
+
+}
