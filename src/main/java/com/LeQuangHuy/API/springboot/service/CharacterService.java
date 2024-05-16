e6cfd99441d91ea46c03d472a2684658ad45bbd0
QuangHuy <lehuyprocuber@gmail.com>
2024-05-16 21:59:47 +0700

    Update CharacterService.java

--- src/main/java/com/LeQuangHuy/API/springboot/service/CharacterService.java
+++ src/main/java/com/LeQuangHuy/API/springboot/service/CharacterService.java
@@ -4,15 +4,6 @@ import com.LeQuangHuy.API.springboot.dto.CharacterDTO;
 
 import java.util.List;
 
-public interface CharacterService {
-
-	List<CharacterDTO> getAllCharacters();
-
+public interface CharacterService extends BaseService<CharacterDTO> {
 	List<CharacterDTO> findByCharacterName(String characterName);
-
-	CharacterDTO updateCharacter(Long id, CharacterDTO updatedCharacterDTO);
-
-	void deleteCharacterById(Long id);
-
-	CharacterDTO saveCharacter(CharacterDTO characterDTO);
 }
