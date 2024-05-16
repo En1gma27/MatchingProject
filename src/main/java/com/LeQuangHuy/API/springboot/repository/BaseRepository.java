96a30f5339256f323c5f46bfc58612b429106910
QuangHuy <lehuyprocuber@gmail.com>
2024-05-16 23:34:11 +0700

    Create BaseRepository.java

---
+++ src/main/java/com/LeQuangHuy/API/springboot/repository/BaseRepository.java
@@ -0,0 +1,11 @@
+package com.LeQuangHuy.API.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.UUID;
+
+@NoRepositoryBean
+public interface BaseRepository<E, ID> extends JpaRepository<E, ID> {
+
+}
