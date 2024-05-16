4308ca840cfa9fee980a175896d0ae8a32162f5b
QuangHuy <lehuyprocuber@gmail.com>
2024-05-16 22:03:24 +0700

    Update CharacterDTO.java

--- src/main/java/com/LeQuangHuy/API/springboot/dto/CharacterDTO.java
+++ src/main/java/com/LeQuangHuy/API/springboot/dto/CharacterDTO.java
@@ -3,10 +3,11 @@ package com.LeQuangHuy.API.springboot.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
-public class CharacterDTO {
-    private Long id;
+public class CharacterDTO extends BaseDTO {
     private String characterName;
     private Long userId; // Thêm userId để tham chiếu đến người dùng liên quan
 }
