37b03a48cf36d71063b036d73db8c22a92c0a62e
QuangHuy <lehuyprocuber@gmail.com>
2024-05-16 22:04:45 +0700

    Update CharacterMapper.java

--- src/main/java/com/LeQuangHuy/API/springboot/mapper/CharacterMapper.java
+++ src/main/java/com/LeQuangHuy/API/springboot/mapper/CharacterMapper.java
@@ -1,14 +1,18 @@
 package com.LeQuangHuy.API.springboot.mapper;
 
 import com.LeQuangHuy.API.springboot.dto.CharacterDTO;
+import com.LeQuangHuy.API.springboot.mapper.BaseMapper;
 import com.LeQuangHuy.API.springboot.model.Character;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface CharacterMapper {
+public interface CharacterMapper extends BaseMapper<Character, CharacterDTO> {
 
+    @Override
     @Mapping(source = "user.id", target = "userId")
-    CharacterDTO characterToDTO(Character character);
-    Character toEntity(CharacterDTO characterDTO);
+    CharacterDTO entityToDTO(Character character);
+
+    @Override
+    Character dtoToEntity(CharacterDTO dto);
 }
