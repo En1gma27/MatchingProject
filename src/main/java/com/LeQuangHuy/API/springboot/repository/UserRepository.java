445e7a73df3df9625437aeceaf241456abc1016d
QuangHuy <lehuyprocuber@gmail.com>
2024-05-16 22:01:47 +0700

    Update UserRepository.java

--- src/main/java/com/LeQuangHuy/API/springboot/repository/UserRepository.java
+++ src/main/java/com/LeQuangHuy/API/springboot/repository/UserRepository.java
@@ -2,14 +2,17 @@ package com.LeQuangHuy.API.springboot.repository;
 
 import com.LeQuangHuy.API.springboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	User findByUsername(String username);
-
-
+	@Query("SELECT u FROM User u WHERE u.username LIKE %:name%")
+	List<User> findByUsername(@Param("name") String username);
 	boolean existsByEmail(String email);
 
 	boolean existsByUsername(String username);
