463acc8f0ce98436cb6baecf40120556245d58c3
QuangHuy <lehuyprocuber@gmail.com>
2024-05-17 13:29:16 +0700

    Create DemandMapper.java

---
+++ src/main/java/com/LeQuangHuy/API/springboot/mapper/DemandMapper.java
@@ -0,0 +1,18 @@
+package com.LeQuangHuy.API.springboot.mapper;
+
+
+import com.LeQuangHuy.API.springboot.dto.DemandDTO;
+import com.LeQuangHuy.API.springboot.model.Demand;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface DemandMapper extends BaseMapper<Demand, DemandDTO> {
+
+    @Override
+    @Mapping(source = "user.id", target = "userId")
+    DemandDTO entityToDTO(Demand demand);
+
+    @Override
+    Demand dtoToEntity(DemandDTO dto);
+}
