7ea4968ff5bd6bea9031fc2573333e0b01ef36bb
QuangHuy <lehuyprocuber@gmail.com>
2024-05-22 19:25:10 +0700

    Update BaseDTO.java

--- src/main/java/com/LeQuangHuy/API/springboot/dto/BaseDTO.java
+++ src/main/java/com/LeQuangHuy/API/springboot/dto/BaseDTO.java
@@ -1,33 +1,11 @@
 package com.LeQuangHuy.API.springboot.dto;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDateTime;
-
+@Getter
+@Setter
 public class BaseDTO {
     private Long id;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+    private Instant createdAt;
+    private Instant updatedAt;
 }
