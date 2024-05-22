8497593819ff8abb5e0b8cec27d26e329f0c15d7
QuangHuy <lehuyprocuber@gmail.com>
2024-05-22 16:57:10 +0700

    Update BaseService.java

--- src/main/java/com/LeQuangHuy/API/springboot/service/BaseService.java
+++ src/main/java/com/LeQuangHuy/API/springboot/service/BaseService.java
@@ -1,10 +1,12 @@
 package com.LeQuangHuy.API.springboot.service;
-
 import com.LeQuangHuy.API.springboot.dto.BaseDTO;
+import com.LeQuangHuy.API.springboot.filter.BaseFilter;
+import com.LeQuangHuy.API.springboot.model.BaseModel;
 
 import java.util.List;
 
-public interface BaseService<DTO extends BaseDTO> {
+public interface BaseService<DTO extends BaseDTO, FT extends BaseFilter, C extends BaseModel, L extends Long> {
+
 
     List<DTO> getAll();
 
