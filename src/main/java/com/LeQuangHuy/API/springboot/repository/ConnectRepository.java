8e4afebfb3ff05ab732079b0689f4c8be4f39815
QuangHuy <lehuyprocuber@gmail.com>
2024-05-22 17:05:10 +0700

    Update ConnectRepository.java

--- src/main/java/com/LeQuangHuy/API/springboot/repository/ConnectRepository.java
+++ src/main/java/com/LeQuangHuy/API/springboot/repository/ConnectRepository.java
@@ -1,14 +1,22 @@
+package com.LeQuangHuy.API.springboot.repository;
+
+import com.LeQuangHuy.API.springboot.filter.ConnectFilter;
 import com.LeQuangHuy.API.springboot.model.Connect;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+@Repository
+public interface ConnectRepository extends BaseRepository<Connect, Long> {
 
-
-public interface ConnectRepository extends JpaRepository<Connect, Long> {
-
-    List<Connect> findByUserId (Long userId);
-    List<Connect> findByType(String type);
-    List<Connect> findByUserIdAndType(Long userId, String type);
-
-
+    @Query("SELECT c FROM Connect c WHERE " +
+            "(:userId IS NULL OR c.user.id = :userId) " +
+            "AND (:id IS NULL OR c.id = :id) " +
+            "AND (:type IS NULL OR c.type = :type) " +
+            "AND (:status IS NULL OR c.status = :status) " +
+            "AND (:groupName IS NULL OR c.groupName = :groupName)")
+    Page<Connect> findWithFilter(Pageable pageable, ConnectFilter filter);
+                              
 }
