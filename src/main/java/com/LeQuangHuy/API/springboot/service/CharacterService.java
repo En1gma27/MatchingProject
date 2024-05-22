989e8adfd5d418b0b48c5d97cef7979603a2fccd
QuangHuy <lehuyprocuber@gmail.com>
2024-05-23 00:39:14 +0700

    Update CharacterService.java

--- src/main/java/com/LeQuangHuy/API/springboot/service/CharacterService.java
+++ src/main/java/com/LeQuangHuy/API/springboot/service/CharacterService.java
@@ -1,9 +1,12 @@
 package com.LeQuangHuy.API.springboot.service;
-
 import com.LeQuangHuy.API.springboot.dto.CharacterDTO;
+import com.LeQuangHuy.API.springboot.filter.CharacterFilter;
+import com.LeQuangHuy.API.springboot.model.Character;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+public interface CharacterService extends BaseService<CharacterDTO, CharacterFilter, Character, Long> {
 
-public interface CharacterService extends BaseService<CharacterDTO> {
-	List<CharacterDTO> findByCharacterName(String characterName);
+    Page<CharacterDTO> findWithFilter(Pageable pageable, Long userId, String characterName);
+
 }
