5d6bd3ff827a3aa3026a481ea1c49f5e5c362d7e
QuangHuy <lehuyprocuber@gmail.com>
2024-05-23 14:30:34 +0700

    Update BaseRepository.java

--- src/main/java/com/LeQuangHuy/API/springboot/repository/BaseRepository.java
+++ src/main/java/com/LeQuangHuy/API/springboot/repository/BaseRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.NoRepositoryBean;
 import java.util.UUID;
 
 @NoRepositoryBean
-public interface BaseRepository<E, ID> extends JpaRepository<E, ID> {
-
+public interface BaseRepository<E, ID> extends JpaRepository<E, ID>, JpaSpecificationExecutor<E> {
+    // Các phương thức khác nếu cần thiết
 }
