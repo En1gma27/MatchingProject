cd52efe7df47aa96d89e3a4817c8f3564b58da0a
QuangHuy <lehuyprocuber@gmail.com>
2024-05-24 15:57:38 +0700

    Update FeedBackController .java

--- src/main/java/com/LeQuangHuy/API/springboot/controller/FeedBackController .java
+++ src/main/java/com/LeQuangHuy/API/springboot/controller/FeedBackController .java
@@ -2,51 +2,49 @@ package com.LeQuangHuy.API.springboot.controller;
 
 import com.LeQuangHuy.API.springboot.dto.ConnectDTO;
 import com.LeQuangHuy.API.springboot.dto.FeedBackDTO;
+import com.LeQuangHuy.API.springboot.filter.FeedBackFilter;
+import com.LeQuangHuy.API.springboot.model.FeedBack;
 import com.LeQuangHuy.API.springboot.service.ConnectService;
 import com.LeQuangHuy.API.springboot.service.FeedBackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.OrderBy;
 import java.util.List;
 @RestController
 @RequestMapping("/feedBacks")
-public class FeedBackController {
+public class FeedBackController extends BaseController<FeedBackDTO, FeedBackFilter, FeedBack,Long> {
     private final FeedBackService feedBackService;
+
 
     @Autowired
     public FeedBackController(FeedBackService feedBackService) {
+        super(feedBackService);
         this.feedBackService = feedBackService;
     }
 
-
     @GetMapping
-    public List<FeedBackDTO> getFeedBacks(@RequestParam(required = false) Long userId,
+    public Page<FeedBackDTO> getFeedBacks(@RequestParam(required = false) Long userId,
                                           @RequestParam(required = false) Integer rate,
-                                          @RequestParam(required = false) String orderBy) {
-        if ("asc".equalsIgnoreCase(orderBy)) {
-            return feedBackService.findAllByOrderByRateAsc();
-        } else if ("desc".equalsIgnoreCase(orderBy)) {
-            return feedBackService.findAllByOrderByRateDesc();
-        } else if (userId != null && rate != null) {
-            return feedBackService.findByUserIdAndRate(userId, rate);
-        } else if (userId != null) {
-            return feedBackService.findByUserId(userId);
-        } else if (rate != null) {
-            return feedBackService.findByRate(rate);
+                                          @RequestParam(defaultValue = "asc") String orderBy,
+                                          @RequestParam(defaultValue = "0") int page,
+                                          @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        FeedBackFilter filter = new FeedBackFilter();
+        if (userId != null || rate != null) {
+            if ("desc".equalsIgnoreCase(orderBy)) {
+                return feedBackService.findWithFilterAndSortByRateDesc(pageable,userId,rate);
+            } else {
+                return feedBackService.findWithFilterAndSortByRateAsc(pageable,userId,rate);
+            }
         } else {
-            return feedBackService.getAll();
+            List<FeedBackDTO> allFeedBacks = feedBackService.getAll();
+            return new PageImpl<>(allFeedBacks, pageable, allFeedBacks.size());
         }
-    }
-    @PostMapping("/feedBacks")
-    public ResponseEntity<FeedBackDTO> createFeedBack(@RequestBody FeedBackDTO feedBackDTO) {
-        FeedBackDTO savedFeedBackDTO = feedBackService.save(feedBackDTO);
-        return ResponseEntity.ok(savedFeedBackDTO);
-    }
-
-
-    @DeleteMapping("/{id}")
-    public void deleteConnectById(@PathVariable Long id) {
-        feedBackService.deleteById(id);
     }
 }
